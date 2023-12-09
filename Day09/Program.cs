e7b25f420c3f51fab8c115efb106e21ccf20d45d
Jay Senich <1821282+jsenich@users.noreply.github.com>
2023-12-09 02:00:03 -0500

    Day 9 part 2 complete

--- Day09/Program.cs
+++ Day09/Program.cs
@@ -2,15 +2,8 @@ using System.Net.Sockets;
 
 internal class Program
 {
-    static int PartOne(string[] puzzleInput)
+    static long PartOne(string[] puzzleInput)
     {
-        return 0;
-    }
-
-    private static void Main(string[] args)
-    {
-        var puzzleInput = File.ReadAllText("input.txt").Trim().Split("\n");
-
         var historyValues = new List<long>();
         int lineNum = 0;
         foreach (var line in puzzleInput)
@@ -47,8 +40,56 @@ internal class Program
             lineNum++;
         }
 
+        return historyValues.Sum();
+    }
+
+    static long PartTwo(string[] puzzleInput)
+    {
+        var historyValues = new List<long>();
+        foreach (var line in puzzleInput)
+        {
+            var nums = line.Split(" ").Select(n => long.Parse(n)).ToList();
+            var histories = new List<List<long>>() { nums };
+
+            int i = 0;
+
+            while (true)
+            {
+                histories.Add(new List<long>());
+                for (int j = 0; j < histories[i].Count - 1; j++)
+                {
+                    histories[i + 1].Add(histories[i][j + 1] - histories[i][j]);
+                }
 
 
-        Console.WriteLine($"Part One: {historyValues.Sum()}"); // Answer: 2043677056
+                if (histories[i + 1].All(x => x == 0))
+                {
+                    break;
+                }
+                i++;
+            }
+
+            for (int x = histories.Count - 1; x > 0; x--)
+            {
+                histories[x - 1].Insert(0, histories[x - 1][0] - histories[x][0]);
+                if (x - 1 == 0)
+                {
+                    historyValues.Add(histories[x - 1][0]);
+                    break;
+                }
+            }
+        }
+
+        return historyValues.Sum();
+    }
+
+    private static void Main(string[] args)
+    {
+        var puzzleInput = File.ReadAllText("input.txt").Trim().Split("\n");
+
+        Console.WriteLine($"Part One: {PartOne(puzzleInput)}"); // Answer: 2043677056
+        Console.WriteLine($"Part Two: {PartTwo(puzzleInput)}"); // Answer: 1062
+
+
     }
 }
