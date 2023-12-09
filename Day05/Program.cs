a75404e21f259c15572c2793621bfc3dd30a712b
Jay Senich <1821282+jsenich@users.noreply.github.com>
2023-12-08 23:55:48 -0500

    Day 5 part 2 WIP

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,6 +1,4 @@
-using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
-using System.Xml;
 
 internal class Program
 {
@@ -54,9 +52,62 @@ internal class Program
         return lowestNum;
     }
 
-    private static ulong PartTwo(string[] puzzleInput)
+    private static ulong PartTwo(string[] almanac)
     {
-        throw new NotImplementedException();
+        ulong lowestNum = 9999999999;
+
+        var maps = new Dictionary<int, List<(ulong dest, ulong src, ulong count)>>();
+        var seeds = Regex.Matches(almanac[0], @"\d+").Select(m => ulong.Parse(m.Value)).ToList();
+        var seedRanges = new List<(ulong start, ulong count)>();
+        for (int i = 0; i < seeds.Count; i = i + 2)
+        {
+            seedRanges.Add((seeds[i], seeds[i + 1]));
+        }
+
+        for (int i = 1; i < almanac.Length; i++)
+        {
+            List<(ulong, ulong, ulong)> destSourceRange = Regex.Matches(almanac[i], @"\d+ \d+ \d+").Select(
+                m => m.Value.Split(" ")
+                switch
+                { var s => (ulong.Parse(s[0]), ulong.Parse(s[1]), ulong.Parse(s[2])) }
+                ).ToList();
+
+            maps[i] = new List<(ulong dest, ulong src, ulong count)>();
+            foreach (var s in destSourceRange)
+            {
+                maps[i].Add((s.Item1, s.Item2, s.Item3));
+            }
+        }
+
+        foreach (var seedRange in seedRanges)
+        {
+            foreach (var seed in Range(seedRange.start, seedRange.count))
+            {
+                var currMap = 1;
+                var currKey = seed;
+                while (currMap < maps.Count + 1)
+                {
+                    var myMap = maps[currMap];
+
+                    foreach (var numRange in myMap)
+                    {
+                        if (currKey >= numRange.src && currKey <= (numRange.src + numRange.count))
+                        {
+                            currKey = numRange.dest + (currKey - numRange.src);
+                            break;
+                        }
+                    }
+
+                    currMap++;
+                }
+                if (currKey < lowestNum)
+                {
+                    lowestNum = currKey;
+                }
+            }
+        }
+
+        return lowestNum;
     }
 
     public static IEnumerable<ulong> Range(ulong start, ulong count)
@@ -69,6 +120,7 @@ internal class Program
         var puzzleInput = File.ReadAllText("input.txt").Trim().Split("\n\n");
 
 
-        Console.WriteLine($"Part One: {PartOne(puzzleInput)}"); // Answer: 382895070
+        // Console.WriteLine($"Part One: {PartOne(puzzleInput)}"); // Answer: 382895070
+        Console.WriteLine($"Part Two: {PartTwo(puzzleInput)}"); // Answer: 382895070
     }
 }
