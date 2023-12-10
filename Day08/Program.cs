56ed31b89192e730718c4a13933621e942deaa54
Jay Senich <1821282+jsenich@users.noreply.github.com>
2023-12-09 22:08:11 -0500

    Day 8 part two complete

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using System.Text.RegularExpressions;
 
 internal class Program
@@ -48,6 +47,18 @@ internal class Program
         return steps;
     }
 
+    static long gcd(long n1, long n2)
+    {
+        if (n2 == 0)
+        {
+            return n1;
+        }
+        else
+        {
+            return gcd(n2, n1 % n2);
+        }
+    }
+
     static long PartTwo(string directions, string instructions)
     {
         var matches = Regex.Matches(instructions, nodePattern, RegexOptions.Multiline);
@@ -60,50 +71,44 @@ internal class Program
             }
             ).ToDictionary(x => x.Key, x => x.Value);
 
-        long steps = 0;
-
         var startNodes = nodeMap.Keys.Where(x => x.EndsWith("A")).ToList();
-        var stopNodes = nodeMap.Keys.Where(x => x.EndsWith("Z")).ToList();
+        var totals = new List<long>();
 
-        var currentNodes = new List<string>(startNodes);
-
-        while (true)
+        foreach (var startNode in startNodes)
         {
-            foreach (var d in directions)
+            int steps = 0;
+            var currentNode = startNode;
+
+            while (!currentNode.EndsWith("Z"))
             {
-                if (d == 'L')
+                foreach (var d in directions)
                 {
-                    currentNodes[0] = nodeMap[currentNodes[0]].Item1;
-                    currentNodes[1] = nodeMap[currentNodes[1]].Item1;
-                    currentNodes[2] = nodeMap[currentNodes[2]].Item1;
-                    currentNodes[3] = nodeMap[currentNodes[3]].Item1;
-                    currentNodes[4] = nodeMap[currentNodes[4]].Item1;
-                    currentNodes[5] = nodeMap[currentNodes[5]].Item1;
-                }
-                else
-                {
-                    currentNodes[0] = nodeMap[currentNodes[0]].Item2;
-                    currentNodes[1] = nodeMap[currentNodes[1]].Item2;
-                    currentNodes[2] = nodeMap[currentNodes[2]].Item2;
-                    currentNodes[3] = nodeMap[currentNodes[3]].Item2;
-                    currentNodes[4] = nodeMap[currentNodes[4]].Item2;
-                    currentNodes[5] = nodeMap[currentNodes[5]].Item2;
-                }
-                steps++;
-                if (currentNodes.Intersect(stopNodes).ToList().Count == 6)
-                {
-                    return steps;
+                    if (d == 'L')
+                    {
+                        currentNode = nodeMap[currentNode].Item1;
+                    }
+                    else
+                    {
+                        currentNode = nodeMap[currentNode].Item2;
+                    }
+                    steps++;
+                    if (currentNode.EndsWith("Z"))
+                    {
+                        break;
+                    }
                 }
             }
+            totals.Add(steps);
         }
+
+        return totals.Aggregate((S, val) => S * val / gcd(S, val));
     }
 
     private static void Main(string[] args)
     {
         var puzzleInput = File.ReadAllText("input.txt").Trim().Split("\n\n");
 
-        // Console.WriteLine($"Part One: {PartOne(puzzleInput[0], puzzleInput[1])}"); // 19631
-        Console.WriteLine($"Part Two: {PartTwo(puzzleInput[0], puzzleInput[1])}"); //
-
+        Console.WriteLine($"Part One: {PartOne(puzzleInput[0], puzzleInput[1])}"); // 19631
+        Console.WriteLine($"Part Two: {PartTwo(puzzleInput[0], puzzleInput[1])}"); // 21003205388413
     }
 }
