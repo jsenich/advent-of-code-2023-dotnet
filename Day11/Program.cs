dcf5af6d95a93dd39217128e7fd63fd790371fd8
Jay Senich <1821282+jsenich@users.noreply.github.com>
2023-12-28 00:18:48 -0500

    Day 11 part two complete

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,8 +1,6 @@
-using System.Diagnostics.Metrics;
-
 internal class Program
 {
-    private static int PartOne(string[] puzzleInput)
+    static int PartOne(string[] puzzleInput)
     {
         var grid = new List<List<char>>();
         var length = puzzleInput[0].Length;
@@ -71,10 +69,66 @@ internal class Program
         return total;
     }
 
+    static long PartTwo(string[] puzzleInput, int multiplier)
+    {
+        var length = puzzleInput[0].Length;
+        var galaxyCols = new HashSet<int>();
+        var colNums = Enumerable.Range(0, length).ToHashSet();
+        var galaxies = new List<(long, long)>();
+        var emptyRows = new List<long>();
+
+        for (int i = 0; i < puzzleInput.Length; i++)
+        {
+            if (!puzzleInput[i].Contains('#'))
+            {
+                emptyRows.Add(i);
+            }
+            for (int j = 0; j < puzzleInput[i].Length; j++)
+            {
+                if (puzzleInput[i][j] == '#')
+                {
+                    galaxyCols.Add(j);
+                }
+            }
+        }
+
+        var emptyColumns = colNums.Except(galaxyCols).Order().ToList();
+
+        for (int i = 0; i < puzzleInput.Length; i++)
+        {
+            for (int j = 0; j < puzzleInput[i].Length; j++)
+            {
+                if (puzzleInput[i][j] == '#')
+                {
+                    long row = emptyRows.Where(x => x < i).Select(_ => multiplier - 1).Sum() + i;
+                    long col = emptyColumns.Where(x => x < j).Select(_ => multiplier - 1).Sum() + j;
+                    galaxies.Add((row, col));
+                }
+            }
+        }
+
+        long total = 0;
+
+        var counter = 0;
+        foreach (var point in galaxies)
+        {
+            foreach (var point2 in galaxies.Skip(counter + 1))
+            {
+                total += Math.Abs((point.Item1 - point2.Item1)) + Math.Abs((point.Item2 - point2.Item2));
+            }
+            counter++;
+        }
+
+        return total;
+    }
+
     private static void Main(string[] args)
     {
         var puzzleInput = File.ReadAllText("input.txt").Trim().Split("\n");
 
         Console.WriteLine($"Part One: {PartOne(puzzleInput)}"); // Answer: 10033566
+
+        var multiplier = 1000000;
+        Console.WriteLine($"Part Two: {PartTwo(puzzleInput, multiplier)}"); // Answer: 560822911938
     }
 }
